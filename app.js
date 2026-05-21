1e3a4b097ffe2495a87d87df309f6e7ae45b8ab9
kenken-ctrl <mky7pwtj4j@privaterelay.appleid.com>
2026-05-21 16:39:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const SUPABASE_URL
-const SUPABASE_ANON_KEY
-const supabaseClient
+const SUPABASE_URL =
+  "https://rsctqdpmfzisfksfrplp.supabase.co";
+
+const SUPABASE_ANON_KEY =
+  "ta clé";
+
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );

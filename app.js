2ca25ed0825afe90f0e044cae2b9a833dff5aba3
kenken-ctrl <mky7pwtj4j@privaterelay.appleid.com>
2026-05-21 16:57:39 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("NOUVEAU APP JS CHARGÉ");
+
+const authBox =
+  document.getElementById("authBox");
+
+const app =
+  document.getElementById("app");
+
+const statusEl =
+  document.getElementById("status");
+
+const logoutBtn =
+  document.getElementById("logoutBtn");
+
+const tableBody =
+  document.getElementById("tableBody");
+
+let currentUser = null;
+let holdings = [];

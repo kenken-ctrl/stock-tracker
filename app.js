629766dda5ccc63a69985f96d70f3e6c07c69d8b
kenken-ctrl <mky7pwtj4j@privaterelay.appleid.com>
2026-05-21 16:39:59 +0200

    Update Supabase anon key in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
   "https://rsctqdpmfzisfksfrplp.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "ta clé";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzY3RxZHBtZnppc2Zrc2ZycGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNTYzMzcsImV4cCI6MjA5NDkzMjMzN30.Cezyi64xbxOJcKHvcgsJFMW67K765zUPgHEGh50hl_k";
 
 const supabaseClient =
   window.supabase.createClient(

8f751c118f18b9c5a3bae5a25b5273812048e072
kenken-ctrl <mky7pwtj4j@privaterelay.appleid.com>
2026-05-21 16:32:54 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL =
-  "TON_URL_SUPABASE";
+  "https://rsctqdpmfzisfksfrplp.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "TA_CLE_SUPABASE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzY3RxZHBtZnppc2Zrc2ZycGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNTYzMzcsImV4cCI6MjA5NDkzMjMzN30.Cezyi64xbxOJcKHvcgsJFMW67K765zUPgHEGh50hl_k";
 
 const supabaseClient =
   window.supabase.createClient(
